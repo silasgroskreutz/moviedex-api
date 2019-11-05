79c03c989d74645ccd4ea1ed93deda273bb2adfe
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-11-05 09:26:28 -0600

    added missing listen statement

--- server.js
+++ server.js
@@ -53,3 +53,7 @@ app.get('/movie', function handleGetMovies(req, res) {
 } )
 
 const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
